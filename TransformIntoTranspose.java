6392da18d0925592ce18d5715aa3d8069b23e813
Devanshu3032 <devanshukumar841@gmail.com>
2025-04-03 00:13:40 +0530

    Experiment

--- TransformIntoTranspose.java
+++ TransformIntoTranspose.java
@@ -12,10 +12,10 @@ class TransformIntoTranspose{
     }
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-        int m = arr.length , n = arr[0].length; 
+        int n = arr.length ;
         print(arr);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j <=i; j++) {
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;

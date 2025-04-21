e5b3b27cc43541554f89448d82d0d249b20059c7
Devanshu3032 <devanshukumar841@gmail.com>
2025-04-21 23:24:02 +0530

    "u"

--- Refreshing.java
+++ Refreshing.java
@@ -3,6 +3,7 @@
 public class Refreshing {
     public static void main(String[] args) {
         int[][] arr= {{1,2,3},{4,5,6},{7,8,9}};
+        int m = arr.length;
         // Scanner sc = new Scanner(System.in);
         // for (int i = 0; i < arr.length; i++) {
         //     for(int j = 0; j < arr[0].length; j++)
@@ -26,6 +27,26 @@ public class Refreshing {
                 }
                 System.out.println();
                 
+            }
+
+            System.out.println();
+            for (int i = 0; i < m; i++) {
+                int a =  0  , b = m-1;
+                // Swaping a[i][a] and arr[i][b]
+             while (a<b) {
+                int temp = arr[i][a] ; 
+                arr[i][a] = arr[i][b];
+                arr[i][b] = temp ;
+                 a++ ;
+                 b-- ;
+                
+             }
+            }
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
             }
             
         }

dd2cf1e25fe1b9492012c3cb90787d5609e3ce6b
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-02 21:27:22 +0530

    "new update "

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -2,16 +2,12 @@ public class SearchinRotatedSortedArray {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
-        int key = 0;
+        int key = -1;
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] == key ){
                 target = i ; 
                 break ;
-            } else 
-            {
-                target = -1;
-                
-            }
+            } 
         }
         System.out.println("The target is found at index: " + target);
     }

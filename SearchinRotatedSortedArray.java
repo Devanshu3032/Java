0e2e254a6d9dc6600239acee635df8faef0e1d7e
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-02 21:30:49 +0530

    "fully optimize "

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -1,8 +1,8 @@
 public class SearchinRotatedSortedArray {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
-        int key = -1;
+        int target = -1;
+        int key = 0;
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] == key ){
                 target = i ; 

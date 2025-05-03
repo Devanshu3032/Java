6b8aebffd138500f63b8fd1fb0be3ebb72216fb6
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-03 23:19:25 +0530

    "n"

---
+++ PeakElementM3.java
@@ -0,0 +1,15 @@
+public class PeakElementM3 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int peak = 0 ; 
+        int max = Integer.MIN_VALUE ;
+        for(int i = 0 ; i < arr.length ; i++){
+           if(arr[i] > max ){
+               max = arr[i] ; 
+               peak = i ;
+           }
+        }
+        System.out.println("Peak Element is: " + peak);
+    }
+}

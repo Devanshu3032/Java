612564468cf863311340afdc872214f4e24c3e63
Devanshu3032 <devanshukumar841@gmail.com>
2025-05-04 23:59:54 +0530

    "g"

--- PeakElementM3.java
+++ PeakElementM3.java
@@ -7,7 +7,7 @@ public class PeakElementM3 {
         for(int i = 0 ; i < arr.length ; i++){
            if(arr[i] > max ){
                max = arr[i] ; 
-               peak = ooo ;
+               peak = oo ;
            }
         }
         System.out.println("Peak Element is: " + peak);

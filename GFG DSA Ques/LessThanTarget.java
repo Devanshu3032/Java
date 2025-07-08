1b7f8e415261d752d93db8f539577d59a103d569
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-08 22:43:10 +0530

    "day52"

--- GFG DSA Ques/LessThanTarget.java
+++ GFG DSA Ques/LessThanTarget.java
@@ -1,3 +1,21 @@
+import java.util.Arrays;
+
 class LessThanTarget{
-    
+    public static void main(String[] args) {
+        int arr[] = {2,5,3,7} ;
+        int target = 8 ;
+        Arrays.sort(arr) ;
+        int i =  0 , j = arr.length -1 , count = 0 ;
+        while (i < j ) {
+            if(arr[i] + arr[j] >= target){
+                j-- ;
+            }
+            else{
+                count += j-i ; 
+                i++ ;
+            }
+            
+        }
+        
+    }
 }
